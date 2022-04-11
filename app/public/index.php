1ab8c5f5fdc4ee4a58e413bc0a542a84b761c7b1
Patryk Kostrzewski <pkostrzewski1@gmail.com>
2022-04-11 19:44:12 +0200

    init repo

---
+++ app/public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump(['test' => 'test']);
+
+phpinfo();
+
+$pdo = new PDO('mysql:dbname=tutorial;host=mysql', 'tutorial', 'secret', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$query = $pdo->query('SHOW VARIABLES like "version"');
+
+$row = $query->fetch();
+
+echo 'MySQL version:' . $row['Value'];
